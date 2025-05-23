85c3506e8b14849013956aa7fbde4392c2df2db6
ctes091x <ctes091x@mma.club.uec.ac.jp>
2025-05-23 17:13:00 +0900

    Create .gitconfig

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
