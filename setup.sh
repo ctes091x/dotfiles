fa6ef364fcc7f55d6cbbb18c6722346792e5b77f
ctes091x <ctes091x@ctes091x.net>
2025-12-21 13:51:22 +0900

    chezmoi init

--- setup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-ln -s ~/dotfiles/.bashrc ~/.bashrc
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
